f6231fa99859b34d9778e5aa4a499144499e53b6
utopia-code <utopiacode.developer@gmail.com>
2022-11-05 22:10:54 +0100

    remove test netlify

--- dist/index.5d9a4947.js
+++
@@ -1,2 +0,0 @@
-const name="world";console.log("Hello world");
-//# sourceMappingURL=index.5d9a4947.js.map
---
+++ dist/index.8f0c9192.js
@@ -0,0 +1,4 @@
+const name = "world";
+console.log(`Hello ${name}`);
+
+//# sourceMappingURL=index.8f0c9192.js.map
